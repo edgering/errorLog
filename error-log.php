a819fdcdd0fa440a6ee532794d46dc3f80ef7512
Jirka Kopřiva <jirka@edgering.org>
2017-02-11 23:30:47 +0100

    Update error-log.php

--- error-log.php
+++ error-log.php
@@ -18,18 +18,18 @@
 function _errorHandlerSave($errno,$errstr,$errline,$errfile)
 {  
   $common = array(512 => "WARNING", 1024 => "NOTICE");
-  $type   = (isset($common[$errno])) ? $common[$errno] : 'ERROR [' . $errno . ']'; 
+  $type   = (isset($common[$errno])) ? $common[$errno] : 'ERROR[' . $errno . ']'; 
     
   $err_file       = (defined("ERR_LOG_FILE"))   ? ERR_LOG_FILE   : 'php_errors.log';
   $err_line_style = (defined("ERR_LINE_STYLE")) ? ERR_LINE_STYLE : 'font-family: Consolas; display: block; font-size: .9em; margin-bottom: 1px; background: #f5f5f5; padding: 3px;'; 
   
   echo '<span style="' . $err_line_style . '"><b>' . $type . "</b> " . $errstr;
-  echo "| line $errline in file " . $errfile . "</span>";
+  echo " ... Line ($errline) " . $errfile . "</span>";
 
   if ($err_file && $err_file != '')
   {            
     $handle = fopen($err_file,"a+");
-    fwrite($handle,date("Y-m-d H:i:s") . " " . $type . " $errstr | " . "Line $errline in file " . $errfile . "\n");
+    fwrite($handle,date("Y-m-d H:i:s") . " " . $type . " " . $errstr . " ... Line ($errline) " . $errfile . "\n");
     fclose($handle);
   }  
 }
